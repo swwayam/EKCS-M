cd046a2eb34348947fd7036c611b2e5c1a989f08
swwayam <codingswayam24@gmail.com>
2023-04-05 11:47:03 +0530

    Task 1

--- Typescript/Masood/task.ts
+++ Typescript/Masood/task.ts
@@ -1,2 +1,9 @@
 // hi
 // 
+
+// Tasks 
+
+// 1. Create bmi calculator 
+// 2. Create a function called as calculate bmi
+// 3. it should accept 2 parameters weight and height
+// 4. it should return the user's bmi
