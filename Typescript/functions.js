fe1edf3aa67bbea1601ac0250df459084001273a
masoodcodingzen <masood.codingzen@gmail.com>
2023-04-12 02:34:29 +0530

    practice

--- Typescript/functions.js
+++ Typescript/functions.js
@@ -4,8 +4,8 @@ var add = function (num1, num2) {
     return num1 + num2;
 };
 console.log(add(10, 11));
-var add2 = function (num1, num2) {
-    return num1 + num2;
+var add2 = function (num1, num2, name) {
+    return num1 + num2 + name;
 };
-console.log(add2(10, 11));
+console.log(add2(10, 11, "Masood"));
 // 21Masood
--- Typescript/variables.ts
+++ Typescript/variables.ts
@@ -1,21 +1,20 @@
-let name = "Masood" // String
-
+let name: string = "Masood"
 // automatically detect kar leta
 
 // jab data type change hota you get an error
 name = 12323
 
-let phoneName: string;
 
+let phoneName: string;
 phoneName = "iphone"
-phoneName = 21321
+phoneName = 21321 // data type changed here
+
 
 let phoneModel: number;
-
 phoneModel = 14
 
-let isProSeries: boolean;
 
+let isProSeries: boolean;
 isProSeries = false
 
 
